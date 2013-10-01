3fa41627e123595c82aaa7dd2be46d7b8a6add67
Jarek Kulikowski <jarek707@gmail.com>
2013-10-01 14:30:35 -0400

    ... quick push ...

--- data/getLocStorage.php
+++ data/getLocStorage.php
@@ -5,6 +5,13 @@
         echo file_get_contents($fileName);
     } else { 
         echo "File doesn't exist :" . $fileName;
+        if ( file_exists($_SERVER['DOCUMENT_ROOT'] ) {
+            echo ' server not exist';
+            var_dump( scandir($_SERVER['DOCUMENT_ROOT']  ));
+        } else {
+            echo ' server path is good ';
+            var_dump( scandir($_SERVER['DOCUMENT_ROOT']  ));
+        }
     }
     flush();
 ?>
