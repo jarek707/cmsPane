345d29e6cf02340a3df6cb5459a75fa6bb2eb7b0
Jarek Kulikowski <jarek707@gmail.com>
2013-10-13 19:56:13 -0400

    $1

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-    if (file_exists('index.html')) {
-        echo ' exist' ;
-    } else {
-        echo ' not exist';
-    }
-
     echo file_get_contents('index.html');
-
-    echo '<pre>';
-    var_dump($_SERVER);
 ?>
