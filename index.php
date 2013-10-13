9b2a8483f1aee94edc312d476ec4948017028a2f
Jarek Kulikowski <jarek707@gmail.com>
2013-10-13 19:53:18 -0400

    $1

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+    readfile('index.html');
+    if (file_exists('index.html') {
+        echo ' exist' ;
+    } else {
+        echo ' not exist';
+    }
     echo '<pre>';
     var_dump($_SERVER);
-    readfile('index.html');
 ?>
